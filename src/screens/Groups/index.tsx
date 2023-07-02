84a2a076f0e0462c8ea08becad71eac7bdf200cb
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-07-02 09:02:12 -0300

    lista de grupos e lista vazia

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
-import React from 'react';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header /*showBackButton*/ />
       <Highlight title='Turmas' subtitle='jogue com a sua turma' />
 
-      <GroupCard title='Galera do Ignite'  />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && {flex: 1}} // isto centraliza a msg
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
+      />
     </Container>
   );
 }
