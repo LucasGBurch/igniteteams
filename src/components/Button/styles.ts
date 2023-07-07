2a588021e486e4c6016ab3f641eabb506321fd7f
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-07-07 15:36:58 -0300

    estilo placeholder e refatoração css helper

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,5 @@
+import styled, { css } from 'styled-components/native';
 import { TouchableOpacity } from 'react-native';
-import styled from 'styled-components/native';
 
 export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY';
 
@@ -14,7 +14,8 @@ export const Container = styled(TouchableOpacity)<Props>`
   min-height: 56px;
   max-height: 56px;
 
-  background-color: ${({theme, type}) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+  background-color: ${({ theme, type }) =>
+    type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
 
   border-radius: 6px;
 
@@ -23,7 +24,9 @@ export const Container = styled(TouchableOpacity)<Props>`
 `;
 
 export const Title = styled.Text`
-  font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-  color: ${({theme}) => theme.COLORS.WHITE};
-  font-family: ${({theme}) => theme.FONT_FAMILY.BOLD};;
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+  `};
 `;
