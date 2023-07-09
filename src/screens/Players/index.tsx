51e87cc242a36c507f63324859a5378cb3ac443a
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-07-09 13:50:44 -0300

    Tela Players e ButtonIcon

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from '@components/Header';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Highlight } from '@components/Highlight';
+
+import { Container } from './styles';
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight 
+        title='Nome da turma'
+        subtitle='adicione a galera e separe os times'
+      />
+
+      <ButtonIcon />
+      
+    </Container>
+  );
+}
